3d077afc50ca032e50e348cdff3b3cc9a0b56b8f
QueenMansaMusa <sheamo84@gmail.com>
2024-02-20 20:07:12 -0800

    Add authentication styling and routing

--- pets-unpeeved/src/components/Authentication/AuthPage.js
+++ pets-unpeeved/src/components/Authentication/AuthPage.js
@@ -1,31 +1,30 @@
 import React, { useState } from 'react';
-import Login from '../Login/Login';
-import Registration from '../Registration/Registration';
+import Login from "../Login/Login";
+import Registration from "../Registration/Registration";
+import '../../styles/components/AuthPage.scss';
 
 function AuthPage() {
-  const [isLoginView, setIsLoginView] = useState(true);
+    const [isLoginView, setIsLoginView] = useState(true);
 
-  return (
-    <div>
-      {isLoginView ? (
-        <div>
-          <Login />
-          <p>
-            Don't have an account?{' '}
-            <button onClick={() => setIsLoginView(false)}>Register here</button>
-          </p>
+    return (
+        <div className="auth-page">
+            {isLoginView ? (
+                <div className="auth-page__content">
+                    <Login />
+                    <p className="auth-page__toggle">
+                        Don't have an account? <button className="auth-page__submit" onClick={() => setIsLoginView(false)}>Register here</button>
+                    </p>
+                </div>
+            ) : (
+                <div className="auth-page__content">
+                    <Registration />
+                    <p className="auth-page__toggle">
+                        Already have an account? <button className="auth-page__submit" onClick={() => setIsLoginView(true)}>Login here</button>
+                    </p>
+                </div>
+            )}
         </div>
-      ) : (
-        <div>
-          <Registration />
-          <p>
-            Already have an account?{' '}
-            <button onClick={() => setIsLoginView(true)}>Login here</button>
-          </p>
-        </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default AuthPage;
