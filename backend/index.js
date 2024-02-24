397a295ff938ff222346fff889159d87ba5fbde6
Amoit-Sherine <sheamo84@gmail.com>
2024-02-24 14:17:17 +0300

    Create authenticate token middleware

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
-
+const jwt = require('jsonwebtoken');
 const app = express();
+require('dotenv').config();
 
-app.use(express.json());
+app.use(express.json());c
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+};
 
 const authRouter = require('./src/auth');
 app.use('/auth', authRouter);
